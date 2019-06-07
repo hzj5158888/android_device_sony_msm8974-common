04b57528479a01375a3c84625f4835560cc8e7db
hzj5158888 <www1693303798@163.com>
2019-06-07 15:13:45 +0800

    re add

--- systemprop.mk
+++ systemprop.mk
@@ -18,7 +18,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.mdpcomp.4k2kSplit=true \
     dev.pm.dyn_samplingrate=1 \
     debug.hwui.use_buffer_age=false \
-    debug.cpurend.vsync=false
+    debug.cpurend.vsync=false \
+    debug.sf.recomputecrop=0 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1
 
 # UI
 PRODUCT_PROPERTY_OVERRIDES += \
